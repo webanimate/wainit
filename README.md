ca573e6bebc9c055648098d176218166f6a70257
Web Animate <60752454+webanimate@users.noreply.github.com>
2020-09-29 12:24:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wainit
+
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+
+Create web animations set repository
---
+++ src/index.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const argv = require('minimist')(process.argv.slice(2))
+const execa = require('execa')
+const ora = require('ora')
+const figlet = require('figlet')
+const inquirer = require('inquirer')
+const sanitize = require('sanitize-filename')
+const githubUsernameRegex = require('github-username-regex')
+let dirname
+
+console.clear()
+console.log('\n\x1b[32m' + figlet.textSync(' Wainit', { font: 'alligator2' }) + '\x1b[0m\n')
+
+let questions = []
+
+if (argv._[0] && argv._[0] !== sanitize(argv._[0])) {
+  console.error('\x1b[31mError!\nProject name \x1b[36m' + argv._[0] + '\x1b[31m must be a valid file name!\x1b[0m')
+  process.exit(1)
+}
+if (!argv._[0]) {
+  questions.push({
+    type: 'input',
+    name: 'projectName',
+    message: 'Project name',
+    validate: (value) => {
+      if (value && value === sanitize(value)) {
+        dirname = path.join(process.cwd(), value)
+        if (fs.existsSync(dirname)) {
+          return '\x1b[31mDirectory \x1b[36m' + value + '\x1b[31m already exists!\x1b[0m'
+        } else {
+          return true
+        }
+      } else {
+        return '\x1b[31mProject name must be a valid file name!\x1b[0m'
+      }
+    },
+  })
+}
+
+const _questions = [
+  {
+    type: 'input',
+    name: 'version',
+    message: 'Version',
+    default: () => {
+      return '1.0.0'
+    },
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'Github username',
+    validate: (value) => {
+      if (githubUsernameRegex.test(value)) {
+        return true
+      } else {
+        return '\x1b[31mEnter a valid github username!\x1b[0m'
+      }
+    },
+  },
+]
+
+questions = [...questions, ..._questions]
+
+inquirer.prompt(questions).then((answers) => {
+  const projectName = argv._[0] || answers['projectName']
+  const questions = [
+    {
+      type: 'input',
+      name: 'repository',
+      message: 'Repository url',
+      default: () => {
+        return 'https://github.com/' + answers['githubUsername'] + '/' + projectName
+      },
+    },
+  ]
+
+  inquirer.prompt(questions).then((answers1) => {
+    // console.log(answers1)
+    // console.log(answers)
+    fs.mkdirSync(dirname)
+    const pkg = {
+      name: answers.projectName,
+      version: answers.version,
+      main: 'index.js',
+      repository: answers1.repository,
+    }
+    const spinner = ora(' Creating web animations project')
+    spinner.start()
+    fs.writeFileSync(path.join(dirname, 'package.json'), `${JSON.stringify(pkg, null, 2)}\n`)
+    fs.writeFileSync(path.join(dirname, 'index.js'), `export default {\n\n}\n`)
+    process.chdir(dirname)
+    // && yarn prettier --check "**/*.*"
+    execa('yarn && yarn add file:c:\\my\\work\\web-animations-set\\ -D && yarn web-animations-set').then(
+      () => {
+        spinner.succeed('\x1b[32mWeb animations project successfully created!\x1b[0m')
+      },
+      (error) => {
+        spinner.fail('\x1b[31mError creating web animations project!\x1b[0m\n')
+        console.log(error)
+      }
+    )
+  })
+})
