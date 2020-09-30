59d73c97813add70803449c24610c393395ddbf6
Web Animate <60752454+webanimate@users.noreply.github.com>
2020-09-30 16:02:24 +0200

    chore: add MIT license

--- src/index.js
+++ src/index.js
@@ -104,6 +104,31 @@ inquirer.prompt(questions).then((answers) => {
     const spinner = ora(' Creating web animations project')
     spinner.start()
     fs.writeFileSync(path.join(dirname, 'package.json'), `${JSON.stringify(pkg, null, 2)}\n`)
+    fs.writeFileSync(
+      path.join(dirname, 'LICENSE'),
+      `MIT License
+
+Copyright (c) ${new Date().getFullYear()} ${answers.author}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+`
+    )
     fs.writeFileSync(path.join(dirname, 'index.js'), `export default {\n\n}\n`)
     process.chdir(dirname)
     let installYarn = ''
